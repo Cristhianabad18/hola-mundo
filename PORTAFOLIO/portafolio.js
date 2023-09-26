8409d977bedff3dbadc7b7eaccf6d17e0ab75f91
Cristhianabad18 <145953462+Cristhianabad18@users.noreply.github.com>
2023-09-25 23:15:21 -0500

    Add files via upload

---
+++ PORTAFOLIO/portafolio.js
@@ -0,0 +1,35 @@
+ let menuVisible = false
+ mostarOcultarMenu = function(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false
+     } else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true
+    }
+ }
+ seleccionar = function(){
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
+efectoHabilidades=function(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills>=300){
+        let habilidades = document.getElementsByClassName("progreso")
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajoenequipo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyectmanagement");
+
+    }
+}
+window.onscroll = function(){
+    efectoHabilidades()
+} 
